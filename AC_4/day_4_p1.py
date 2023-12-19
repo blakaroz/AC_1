322b92b5488d21d7f8316997dfabba8077380737
„blakaroz” <karolina.blazytko@gmail.com>
2023-12-19 21:30:04 +0100

    day_4 p1

---
+++ AC_4/day_4_p1.py
@@ -0,0 +1,47 @@
+total = 0
+
+with open("data.txt") as file:
+    for x in file:
+        x = x.split(":")[1].strip()
+        winning_num, my_num = x.split("|")
+        winning_num = list(map(int,winning_num.split()))
+        my_num = list(map(int, my_num.split()))
+       
+        score = sum(num in winning_num for num in my_num)
+        if score > 0:
+            total += 2 **(score-1)
+print(total)
+
+
+
+
+
+
+# import re
+
+
+# with open("data.txt") as file:
+#     lines = file.read().strip().split("\n")
+
+
+#     # print(lines)
+#     ans = 0
+#     for line in lines:
+#         games = re.split("\s+",line)
+#         winning_numbers = [int(num) for num in games[2:12]]
+#         my_numbers = [int(num) for num in games[13:]]
+        
+#         score = 0
+#         score = sum(num in my_numbers for num in winning_numbers)
+#         # for num in my_numbers:
+#         #     if num in winning_numbers:
+#         #         score += 1
+        
+#         if score > 0:
+#             ans += 2 ** (score-1)
+#         # print(score)
+#     print(ans)
+
+
+
+
