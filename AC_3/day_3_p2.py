369810fe1432e62433cdd90edddebc42501df233
„blakaroz” <karolina.blazytko@gmail.com>
2023-12-19 09:43:09 +0100

    day 3 completed

--- AC_3/day_3_p2.py
+++ AC_3/day_3_p2.py
@@ -2,20 +2,15 @@ with open("data.txt") as file:
     data = file.read()
     grid = data.split('\n')
 
-
-# 1. search for coordinates of symbol (not a '.')
-# 2. check if around isdigit (if not just continue searching)
-# 3. find start of the string of digit (numbers)
-# 4. find whole string of digit
-# 5. find the total sum of all that numbers 
-
-
-cs = set()
+total = 0
 
 for r, row in enumerate(grid):
     for c, char in enumerate(row):
-        if char.isdigit() or char == ".":
+        if char != "*":
             continue
+
+        cs = set()
+
         for cr in [r-1,r,r+1]:
             for cc in [c-1,c,c+1]:
                 if cr <0 or cr >= len(grid) or cc <0 or cc >=len(grid[cr]) or not grid[cr][cc].isdigit():
@@ -24,19 +19,19 @@ for r, row in enumerate(grid):
                     cc -=1
             
                 cs.add((cr,cc))
+        if len(cs) != 2:
+            continue
 
 
 
-
-total = 0
-for r,c in cs:
-    string = ""
-    while c < len(grid[r]) and grid[r][c].isdigit():
-        string += grid[r][c]
-        c += 1
-    total += int(string)
-
-
+        list = []
+        for row,column in cs:
+            string = ""
+            while column < len(grid[row]) and grid[row][column].isdigit():
+                string += grid[row][column]
+                column += 1
+            list.append(int(string))
+        total += list[0] * list[1]
 print(total)
 
                 
