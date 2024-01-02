d8707f8d29763f8fed03a5c8b72a4969b666f8ba
„blakaroz” <karolina.blazytko@gmail.com>
2024-01-02 22:20:56 +0100

    completed day 3 in python (still need to improve this code)

--- 2022/AoC_1/main.js
+++ 2022/AoC_1/main.js
@@ -1,8 +1,21 @@
 var fs = require('fs');
-fs.readFile('input_ex.txt', function (err, data) {
-  if (err) return console.error(err);
-  console.log(data.toString());
-});
+const fileContent = fs.readFileSync('input_ex.txt', 'utf8');
+const groups = fileContent.trim().split('\n\n'); 
+
+console.log(groups)
+
+const data = groups.map(group => group.split("\n").map(Number));
+
+console.log(data)
+// console.log()
+
+let sumOfGroups = data.map(group => group.reduce((acc, cv) => acc+ cv, 0));
+console.log(sumOfGroups)
+// const maxSum = Math.max(data.map(group => group.reduce((acc, cv) => acc+ cv, 0)))
+const maxSum = Math.max(...sumOfGroups);
+console.log(maxSum)
 
 
+// Part 2
 
+const sums = data.map(group => group.reduce((acc,cv) => acc + cv, 0));
