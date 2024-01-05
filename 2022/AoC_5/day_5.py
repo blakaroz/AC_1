251d2d1c201d8c69bfaccb5e568545bfbc0bec19
„blakaroz” <karolina.blazytko@gmail.com>
2024-01-05 22:41:29 +0100

    day 5 completed in python

--- 2022/AoC_5/day_5.py
+++ 2022/AoC_5/day_5.py
@@ -1,44 +1,65 @@
-with open("input_ex.txt") as file:
+with open("input.txt") as file:
     lines = file.read().split("\n")
 
-# print(data)
-
-# lines = [line.split(",") for line in data]
-# print(lines)
-
-# from input lines set conteners_dict with will look like:
-# {move: 1,3,2,1,
-# from: 2,1,2,1,
-# to:1,3,1,2}  
+ 
 operation_dict = {"move": [], "from": [], "to": []}
 
 for line in lines:
-    print(line)
+    # print(line)
     move, how_many, froom, where_from, to, where_to = line.split()
     operation_dict["move"].append(how_many)
     operation_dict["from"].append(where_from)
     operation_dict["to"].append(where_to)
     
-print(operation_dict)
+# print(operation_dict)
 
-conteners_dict = {1: ["N", "Z"],
-        2: ["M", "C", "D"],
-        3: ["P"]}
+conteners_dict_no = [["Z", "N"],["M", "C", "D"],["P"]]
+
+conteners_dict = [["F","T","C","L","R","P","G","Q"],
+    ["N","Q","H","W","R","F","S","J"],
+    ["F","B","H","W","P","M","Q"],
+    ["V","S","T","D","F"],
+    ["Q","L","D","W","V","F","Z"],
+    ["Z","C","L","S"],
+    ["Z","B","M","V","D","F"],
+    ["T","J","B"],
+    ["Q","N","B","G","L","S","P","H"]]
+
 
 # print(conteners_dict)
-for i in range(len(operation_dict["move"])):
-    where_from = int(operation_dict["from"][i])
-    how_many = int(operation_dict["move"][i])
-    where_to = int(operation_dict["to"][i])
 
-    if how_many <= len(conteners_dict[where_from]):
-        remove = conteners_dict[where_from][-how_many:]
-        conteners_dict[where_from] = conteners_dict[where_from][:-how_many]
-        conteners_dict[where_to].extend(remove)
-print(conteners_dict)
+for i in range(len(operation_dict["move"])):
+    where_from = int(operation_dict["from"][i]) - 1
+    how_many = int(operation_dict["move"][i])
+    where_to = int(operation_dict["to"][i]) - 1
+
+
+
+#Part 1
+    # for box in range(how_many):
+    #     print(box)
+    #     remove = conteners_dict[where_from].pop()
+    #     conteners_dict[where_to].append(remove)
+
+#Part 2
+    # for box in range(how_many):
+    remove = conteners_dict[where_from][-how_many:]
+    
+    conteners_dict[where_from] = conteners_dict[where_from][:-how_many]
+    conteners_dict[where_to].extend(remove)
+   
+
+
+# print(conteners_dict)
 
 
 ans = ""
-for value in conteners_dict.values():
-    ans += value[-1]
+for container in conteners_dict:
+    if container:  
+        ans += container[-1]
 print(ans)
+
+
+# print(conteners_dict)
+
+
