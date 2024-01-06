17c7bed51b156d6ef4ac14d2e6781e15a41c4ed8
„blakaroz” <karolina.blazytko@gmail.com>
2024-01-06 16:18:00 +0100

    day_6 completed in python

---
+++ 2022/AoC_6/day_6.py
@@ -0,0 +1,19 @@
+with open("input.txt") as file:
+    data=file.read()
+
+print(data)
+
+i = 0
+found = False
+while not found:
+
+    for i in range(len(data)):
+        chars_box = data[i:i+14]
+        #check if next 4 chars are different
+        if len(chars_box) == len(set(chars_box)):
+            print(chars_box)
+            idx = data.find(chars_box) +14
+            print(idx)
+            found = True
+            break
+               
